d9436191ef05ccb722b3a43e6dac6f1be109e533
ohmnapt <napats.work@gmail.com>
2021-12-08 01:14:28 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home component unmount");
+  },
+  activated() {
+    console.log("component activated");
+  },
+  deactivated() {
+    console.log("component deactivated");
+  },
+};
+</script>
